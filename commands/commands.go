bec85d59036701c5156805c76f7a2b0737547044
John Andersen <johnandersenpdx@gmail.com>
2015-09-04 00:14:11 -0700

    Frontend working as well
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- commands/commands.go
+++ commands/commands.go
@@ -4,20 +4,22 @@ import (
 	"github.com/spf13/cobra"
 
 	crypto "github.com/authit/crypto/commands"
+	frontend "github.com/authit/frontend/commands"
 )
 
 var Commands = []*cobra.Command{
 	&cobra.Command{
 		Use:   "crypto",
 		Short: "Provided by authit/crypto",
-		Run: func(cmd *cobra.Command, args []string) {
-			crypto.ConfigBindFlags(cmd)
-			// sample.Run()
-		},
+	},
+	&cobra.Command{
+		Use:   "frontend",
+		Short: "Provided by authit/frontend",
 	},
 }
 
 func init() {
 	ConfigDefaults(Commands...)
 	Commands[0].AddCommand(crypto.Commands...)
+	Commands[1].AddCommand(frontend.Commands...)
 }
--- commands/options.go
+++ commands/options.go
@@ -2,8 +2,10 @@ package commands
 
 import (
 	crypto "github.com/authit/crypto/commands"
+	frontend "github.com/authit/frontend/commands"
 )
 
 var ConfigOptions = map[string]interface{}{
 	"crypto":  crypto.ConfigOptions,
+	"frontend":  frontend.ConfigOptions,
 }
